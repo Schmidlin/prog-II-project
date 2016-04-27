93b77bb0872093701297a41c247a0f99df293bd1
fabiika <fabii@windwslive.com>
2016-04-27 14:11:37 +0200

    test

--- src/main/java/ch/fhnw/project/App.java
+++ src/main/java/ch/fhnw/project/App.java
@@ -10,7 +10,7 @@ public final class App extends Application {
     @Override
     public void start(Stage stage) {
         // Asserts correct project setup (will only compile with Java 8)
-        Supplier<String> helloWorldSupplier = () -> "Hello Fabi";
+        Supplier<String> helloWorldSupplier = () -> "Hello Thoemeli";
         System.out.println(helloWorldSupplier.get());
     }
 
